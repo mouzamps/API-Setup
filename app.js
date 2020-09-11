b7fe00b0276f884a06f133f02c3527edfcc16843
mouzam <mzmjuly9@gmail.com>
2020-09-11 22:59:12 +0530

    created files and setuped node and coded for server and db connection

---
+++ app.js
@@ -0,0 +1,31 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+mongoose.connect('mongodb://localhost:27017/wikiDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const articleschema = new mongoose.Schema({
+    title: String ,
+    content: String
+});
+const Article = mongoose.model("Article",articleschema);
+
+
+
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+  });
+  
